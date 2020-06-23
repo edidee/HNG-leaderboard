152d168bdc756725b5c52091b2d3316d13fa7047
AlexJake660 <uwaobayagbona@gmail.com>
2020-06-23 01:06:45 +0100

    refactor to async/await

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
-function fetchEntries() {
+async function fetchEntries() {
   const url =
     "https://spreadsheets.google.com/feeds/list/17_1B-7knKNEe76EKSwrPaucg9wx36L8HowSiLQz_Pxc/od6/public/values?alt=json";
 
   //get JSON data from API and save as JSON
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => logData(json));
+  var response = await fetch(url);
+  var data = await response.json();
+  var participants = logData(data);
+  //TODO: remove console.log before deployment
+  console.log(participants);
+  var sortedParticipants = sortPoints(participants);
+  //TODO: remove console.log before deployment
+  console.log(sortedParticipants);
+  return sortedParticipants;
 }
 
 //create array of objects from data
@@ -21,8 +27,27 @@ const logData = (data) => {
       });
     });
   }
-  console.log(participants);
   return participants;
 };
+
+//sort array with points property
+function sortPoints(participants) {
+  var sortedParticipants = Object.create(participants);
+
+  function compare(a, b) {
+    const point1 = a.points;
+    const point2 = b.points;
+
+    let comparison = 0;
+    if (point1 > point2) {
+      comparison = -1;
+    } else if (point1 < point2) {
+      comparison = 1;
+    }
+    return comparison;
+  }
+
+  return sortedParticipants.sort(compare);
+}
 
 fetchEntries();
